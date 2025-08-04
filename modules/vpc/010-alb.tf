177cb9439e87dd54033c07c81453ffcc7e1e6148
Vincenzo Buonvino <v.buonvino@reply.it>
2025-08-04 12:19:52 +0200

    Added WAF configuration and ALB to logic

--- modules/vpc/010-alb.tf
+++ modules/vpc/010-alb.tf
@@ -1,43 +1,49 @@
- resource "aws_lb" "dflight-alb" {
-   name               = "dflight-alb"
-   internal           = false
-   load_balancer_type = "application"
-   security_groups    = [aws_security_group.public.id]
-   subnets            = [for subnet in aws_subnet.public : subnet.id]
- }
+resource "aws_lb" "dflight-alb" {
+  name               = "dflight-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.public.id]
+  subnets            = [for subnet in aws_subnet.public : subnet.id]
 
- resource "aws_lb_listener" "dflight-listener" {
-   load_balancer_arn = aws_lb.dflight-alb.arn
-   port              = "80"
-   protocol          = "HTTP"
-   default_action {
-     type             = "forward"
-     target_group_arn = aws_lb_target_group.dflight-tg-eks.arn
-   }
+  tags = local.alb_tags
+}
 
-   depends_on = [aws_lb_target_group.dflight-tg-eks]
- }
+resource "aws_lb_listener" "dflight-listener" {
+  load_balancer_arn = aws_lb.dflight-alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.dflight-tg-eks.arn
+  }
 
- resource "aws_lb_target_group" "dflight-tg-eks" {
-   name     = "dflight-tg-eks"
-   port     = 8000
-   protocol = "HTTP"
-   vpc_id   = aws_vpc.this.id
-   target_type = "ip"
-   health_check {
-     path                = "/health"
-     interval            = 30
-     timeout             = 5
-     healthy_threshold   = 2
-     unhealthy_threshold = 3
-     protocol           = "HTTP"
-     port               = "80" 
-   }
+  depends_on = [aws_lb_target_group.dflight-tg-eks]
+  tags       = local.alb_tags
+}
 
-   lifecycle {
-     create_before_destroy = true
-   }
- }
+resource "aws_lb_target_group" "dflight-tg-eks" {
+  name        = "dflight-tg-eks"
+  port        = 8000
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.this.id
+  target_type = "ip"
+
+  health_check {
+    path                = "/health"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+    protocol            = "HTTP"
+    port                = "traffic-port"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = local.alb_tags
+}
 
 #resource "aws_security_group_rule" "alb_to_private_logic" {
 #  type                     = "egress"
--- modules/vpc/011-waf.tf
+++ modules/vpc/011-waf.tf
@@ -1,12 +1,97 @@
-# resource "aws_wafv2_web_acl" "name" {
-#   scope = "REGIONAL"
-#   name  = "demo-dflight-waf"
-#   visibility_config {
-#     cloudwatch_metrics_enabled = false
-#     metric_name                = "friendly-metric-name"
-#     sampled_requests_enabled   = false
-#   }
-#   default_action {
-#     allow {} #block
-#   }
-# }
+# DFlight WAF v2 Web ACL with AWS Managed Rules
+resource "aws_wafv2_web_acl" "dflight_waf" {
+  name  = "dflight-waf"
+  scope = "REGIONAL"
+
+  default_action {
+    allow {}
+  }
+
+  # AWS Managed Rules - Common Rule Set (Baseline Protection)
+  rule {
+    name     = "AWS-AWSManagedRulesCommonRuleSet"
+    priority = 1
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesCommonRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  # AWS Managed Rules - Known Bad Inputs Rule Set
+  rule {
+    name     = "AWS-AWSManagedRulesKnownBadInputsRuleSet"
+    priority = 2
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesKnownBadInputsRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesKnownBadInputsRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  # Rate-Based Rule (DoS Protection Lite)
+  rule {
+    name     = "RateLimitRule"
+    priority = 3
+
+    action {
+      block {}
+    }
+
+    statement {
+      rate_based_statement {
+        limit              = 1000
+        aggregate_key_type = "IP"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "RateLimitRule"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "DFlightWAF"
+    sampled_requests_enabled   = true
+  }
+
+  tags = local.waf_tags
+}
+
+# Associate WAF with Application Load Balancer
+resource "aws_wafv2_web_acl_association" "dflight_waf_alb_association" {
+  resource_arn = aws_lb.dflight-alb.arn
+  web_acl_arn  = aws_wafv2_web_acl.dflight_waf.arn
+
+  depends_on = [
+    aws_lb.dflight-alb,
+    aws_wafv2_web_acl.dflight_waf
+  ]
+}
